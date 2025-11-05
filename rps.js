de3fecdeb062f8402b9a088ad00f8de5bc02773d
moxxii <moxxiiiiiii0212@gmail.com>
2025-11-05 16:44:34 +1100

    Added win/lose conditions for play-round
    Structured with psuedo-code, given console.log results.

--- rps.js
+++ rps.js
@@ -19,9 +19,45 @@ function getComputerChoice(input){
 }
 
 //Create the name of the function
+//humanChoice needs to be case insensitive
 function getHumanChoice(input) {
     //Create and Return value for user input
-    let humanChoice = prompt("Rock, Paper or Scissors?")
+    let humanChoice = prompt("Rock, Paper or Scissors?").toLowerCase()
     console.log(humanChoice)
 }
 
+//Declare Variables for Player and Computer Score
+let humanScore =  0
+let computerScore = 0
+
+//Let's Begin structuring the Round
+//Time to create a new function
+function playRound(humanChoice, computerChoice) {
+    // Win Condition needs to be defined
+    // Increment humanScore or computerScore based on win.
+    // rock beats scissors, ties to rock, loses to paper
+    if (humanChoice == rock & computerChoice == scissors) {
+        console.log("You win! Rock beats Scissors!");
+    } else if (humanChoice == rock & computerChoice == rock) {
+        console.log("It's a tie!");
+    } else if (humanChoice == rock & computerChoice == paper) {
+        console.log("You lose! Paper beats Rock!"); }
+
+    // scissors beats paper, ties to scissors, loses to rock
+    if (humanChoice == scissors & computerChoice == paper) {
+        console.log("You win! Scissors beats Paper!");
+    } else if (humanChoice == scissors & computerChoice == scissors) {
+        console.log("It's a tie!");
+    } else if (humanChoice == scissors & computerChoice == rock) {
+        console.log("You lose! Rock beats Scissors!")
+    }
+
+    // paper beats rock, ties to paper and loses to scissors
+    if (humanChoice == paper & computerChoice == rock) {
+        console.log("You win! Paper beats Rock!");
+    } else if (humanChoice == paper & computerChoice == paper) {
+        console.log("It's a tie!");
+    } else if (humanChoice == paper & computerChoice == scissors) {
+        console.log("You lose!")
+    }
+}
